69212c27f7a193a99559098e0dde3a9bcb4a5134
AstralSol <porphyrios33@gmail.com>
2020-11-04 17:22:11 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User  # inherits from th user class, first name an last name 
 
+    def initialize
+        @knowledge = []  # initialized wit emtpy array of knowledge
+    end
+
+    def learn(knowledge)  # takes in an argument of a string of knowledge
+        @knowledge << knowledge # an adds it to th students knowledge array
+    end
+
+    def knowledge  # returns that students knowledge array
+        @knowledge
+    end
 end
